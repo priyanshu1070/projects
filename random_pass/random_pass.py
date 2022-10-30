50bb23973999566e1b76df270e158c85bf4f8fd5
0x963 <0x963@no.email>
2022-10-31 00:23:46 +0530

    Fixed password generator

--- random_pass/random_pass.py
+++ random_pass/random_pass.py
@@ -1,4 +1,5 @@
 import random
-passlen = int(input("enter the length of password"))
-s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+passlen = int(input("Enter the length of password "))
+s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+p = "".join(random.sample(s, passlen))
 print(p)
